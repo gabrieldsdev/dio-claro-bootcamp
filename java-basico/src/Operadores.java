24fc7341b0b0744e7f54668cca20a2cdedbf17bc
gabrieldsdev <gabrieldutradev@gmail.com>
2024-08-29 18:42:47 -0300

    Update de arquivos

---
+++ java-basico/src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+        System.out.println(nomeCompleto);
+        System.out.println("---------------");
+        System.out.println(1+1+1+"1");
+        System.out.println(1+"1"+1+1);
+        System.out.println(1+"1"+1+"1");
+        System.out.println("1"+1+1+1);
+        System.out.println("1"+(1+1+1));
+        System.out.println("---------------");
+        int numero = 5;
+        numero = - numero;
+        System.out.println(numero);
+        numero = numero * -1;
+        System.out.println(numero);
+
+    }
+}
--- java-basico/src/TiposVariaveis.java
+++ java-basico/src/TiposVariaveis.java
@@ -1,2 +1,21 @@
 public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        long numeroLongo = 10000L;
+        float numeroDecimal = 150.684F;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        //Constantes devem ser declaradas em caixa alta e com o final
+        final double VALOR_DE_PI = 3.14;
+    }
 }
