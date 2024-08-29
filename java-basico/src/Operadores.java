472f5c6acc36bb14df156165c47acd83ad3d79ca
gabrieldsdev <gabrieldutradev@gmail.com>
2024-08-29 20:13:57 -0300

    Update de arquivos

--- java-basico/src/Operadores.java
+++ java-basico/src/Operadores.java
@@ -14,6 +14,17 @@ public class Operadores {
         System.out.println(numero);
         numero = numero * -1;
         System.out.println(numero);
-
+        System.out.println("---------------");
+        //Ternário
+        int a, b;
+        a = 6;
+        b = 6;
+        String resultado = a == b ? "verdadeiro" : "falso";
+        System.out.println(resultado);
+        System.out.println("---------------");
+        // Comparação de objetos utilizando equals
+        String nomeUm = "Gabriel";
+        String nomeDois = "Gabriel";
+        System.out.println(nomeUm.equals(nomeDois));
     }
 }
