db3d063f914a443f78c9a0ca325f42bcb9c7e300
gabrieldsdev <gabrieldutradev@gmail.com>
2024-09-07 08:44:23 -0300

    Update List

---
+++ Collections/src/list/Ordenacao/exercicio/OrdenacaoPessoas.java
@@ -0,0 +1,44 @@
+package list.Ordenacao.exercicio;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+    private List<Pessoa> listaPessoas;
+
+    public OrdenacaoPessoas() {
+        this.listaPessoas = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        listaPessoas.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        //Duplicando a listaPessoas
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(listaPessoas);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(listaPessoas);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+        ordenacaoPessoas.adicionarPessoa("Nome 1", 20, 1.56);
+        ordenacaoPessoas.adicionarPessoa("Nome 2", 30, 1.80);
+        ordenacaoPessoas.adicionarPessoa("Nome 3", 25, 1.70);
+        ordenacaoPessoas.adicionarPessoa("Nome 4", 17, 1.56);
+
+        System.out.println(ordenacaoPessoas.listaPessoas);
+        System.out.println(ordenacaoPessoas.ordenarPorIdade());
+        System.out.println(ordenacaoPessoas.ordenarPorAltura());
+    }
+
+}
